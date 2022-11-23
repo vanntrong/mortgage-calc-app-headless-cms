b12ad68a77f371a939d5824b8f5d15ffb4952953
vanntrong <vantrongbrv@gmail.com>
2022-11-23 23:31:39 +0700

    feat: init

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,19 +3,24 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `mortgage-calc-app`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "8NBUkKOFVzJkhWHYV5SY5gFcYOnBKgFycHxLHBJZzLo",
-      "spaceId": "g0tz4n5ylm2z"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "bRsaMG-sMXyr-CuP5-v5nME9q4ehEpN_W9YZTCLOqr4",
+        spaceId: "g0tz4n5ylm2z",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+  ],
 };
 
 export default config;
