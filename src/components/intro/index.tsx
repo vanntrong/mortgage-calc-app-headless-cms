9f03824db728b41f662e030f1d2a4e2ee970a7d0
vanntrong <vantrongbrv@gmail.com>
2022-11-24 13:13:19 +0700

    feat: update ui intro

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -27,10 +27,10 @@ const Intro = () => {
       <h2 className="max-w-[679px] mx-auto text-center font-black text-[40px] lg:text-[60px] leading-[140%] mb-[60px] text-[#27292E]">
         {nodes[0].title}
       </h2>
-      <div className="px-[50px] xl:px-[151px] flex-wrap flex gap-[35px]">
+      <div className="px-[50px] xl:px-[151px] flex-wrap lg:flex-nowrap flex gap-[35px]">
         {nodes[0].items.map((item) => (
           <div
-            className="bg-white lg:basis-1/3 py-[50px] px-[35px]"
+            className="bg-white basis-full lg:basis-1/3 py-[50px] px-[35px]"
             style={{ boxShadow: "0px 4px 120px rgba(58, 86, 78, 0.07)" }}
             key={item.id}
           >
